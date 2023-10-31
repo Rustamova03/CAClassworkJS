fc589d70e77d4f2df5aceebc10e7bdd422b6249c
Rustamova Briliant <briliantrustmova@gmail.com>
2023-10-31 13:57:13 +0400

    js

---
+++ js-task1/app.js
@@ -0,0 +1,59 @@
+// let userweather = 24;
+
+// if (userweather >= 22) {
+//   console.log("Nazik geyin");
+// } else if (userweather >= 15) {
+//   console.log("Pencek geyin");
+// } else {
+//   console.log("Qalin geyin");
+// }
+
+// let char = "e";
+
+// if (char == "a" || char == "e" || char == "o" || char == "i") {
+//   console.log("saitdir");
+// }
+// else if(char =="b" || char =="c" || char =="d"){
+//     console.log("samitdir");
+// }
+// else {
+//     console.log("Bu ad uygun deyil")
+// }
+
+// let a = 25;
+// let b = 52;
+// let c = 40;
+
+// if (a > b && a > c) {
+//   console.log(`${a} is greater`);
+// } else if (b > c && b > a) {
+//   console.log(`${b} is greater`);
+// } else {
+//   console.log(`${c} is greater`);
+// }
+
+// let grade = 60;
+
+// if (grade > 90) {
+//   console.log(`A`);
+// } else if (grade > 80) {
+//   console.log(`B`);
+// } else if (grade > 70) {
+//     console.log(`C`)
+// }
+// else if(grade>60){
+//     console.log(`D`)
+// }
+// else if(grade>50){
+//     console.log(`E`)
+// }
+// else{
+//     console.log(`heyiffff`)
+// }
+
+let firstName ="bnam"
+console.log(firstName.length)
+
+if ( firstName.length<5){
+    console.log(`Salam ${firstName} ,Qisa adiniz var`)
+}
