d699ecb82f21d6a9b63524f5ddd05de4d55ca91f
Rustamova Briliant <briliantrustmova@gmail.com>
2023-11-03 13:56:06 +0400

    array

---
+++ Array&Object/app.js
@@ -0,0 +1,28 @@
+// Task 1
+// Verilmis array-da en boyuk ve en balaca elementi tapin.   en boyuk reqem:99, en kicik reqem:6
+const numbers=[12, 45 ,6, 78, 34, 99, 23];
+
+let max=numbers[0];
+let min=numbers[0];
+for(let i=1;i<numbers.length;i++){
+    if(numbers[i]>max){
+        max=numbers[i];
+    }
+    else if(numbers[i]<min){
+        min=numbers[i];
+    }  
+    
+}
+console.log(max,min);
+
+// Task 2 
+// verilmis arraylarin cemini tapin
+let number=[12,45,6,78,34,99,23];
+
+let sum=0;
+for(let i=0,i<number.length,)
+
+
+
+
+
