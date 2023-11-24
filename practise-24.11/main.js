dec27a9524c2600b8cd8fc0d4824682d19fe52fc
Rustamova Briliant <briliantrustmova@gmail.com>
2023-11-24 13:00:45 +0400

    task

---
+++ practise-24.11/main.js
@@ -0,0 +1,8 @@
+let keys = document.querySelectorAll(".key");
+
+window.addEventListener("keydown", function (event) {
+  //   console.log(event);
+  keys.forEach((key) => {
+    console.log();
+  });
+});
