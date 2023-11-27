f882d2b3beccad92dc8737f44da6e6342a9cb94e
Rustamova Briliant <briliantrustmova@gmail.com>
2023-11-27 13:55:47 +0400

    storage

---
+++ JS-storage/app.js
@@ -0,0 +1,15 @@
+// let fullName="Briliant";
+// let age=20;
+// let skills=["walk"]
+
+
+// localStorage.setItem("fullName", "GWP102");
+//  console.log(localStorage.getItem("fullName")); 
+
+// localStorage.setItem("age", "20");
+//  console.log(localStorage.getItem("age")); 
+
+// localStorage.setItem("skills", "walk");
+//  console.log(localStorage.getItem("skills")); 
+
+
---
+++ JS-storage/home.js
@@ -0,0 +1,22 @@
+const form=document.querySelector("#form");
+const fName=document.querySelector("#firstname")
+const lName=document.querySelector("#lastName")
+const address=document.querySelector("#address")
+
+let data =JSON.parse(localStorage.getItem("personObj")) || [];
+
+form.addEventListener("submit", function(e){
+e.preventDefault();
+let info ={
+    id:Date.now(),
+    firstName:fName.ariaValue,
+    lastName:lName.value,
+    address:address.value,
+};
+data.push(info)
+localStorage.setItem("personObj", JSON.stringify(data));
+
+fName.value="",
+lName.value="",
+address.value=""
+});
