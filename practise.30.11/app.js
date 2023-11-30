aea86a5ddf55de230e5f511050b4146168ecb25a
Rustamova Briliant <briliantrustmova@gmail.com>
2023-11-30 12:57:43 +0400

    changed

--- practise.30.11/app.js
+++ practise.30.11/app.js
@@ -2,41 +2,37 @@ let Form = document.querySelector("form");
 let Title = document.querySelector(".title");
 let Login = document.querySelector(".login");
 let Signup = document.querySelector(".signup");
+let name = document.querySelector(".name");
 let Address = document.querySelector(".address");
 let Password = document.querySelector(".password");
+let confirmPas = document.querySelector(".confirmpas");
 let LoginBtn = document.querySelector(".loginBtn");
 
 Form.style.backgroundColor = "white";
 Form.style.padding = "30px";
 Login.style.padding = "5px 28px";
 Login.style.borderRadius = "5px ";
-Login.style.backgroundColor = "blue ";
-Login.style.color = "white ";
 Signup.style.padding = "5px 28px";
 Signup.style.borderRadius = "5px ";
+Signup.style.backgroundColor = "blue ";
+Signup.style.color = "white ";
+name.style.padding = "5px 15px";
+name.style.borderRadius = "5px ";
 Address.style.padding = "5px 15px";
 Address.style.borderRadius = "5px ";
 Password.style.padding = "5px 15px";
 Password.style.borderRadius = "5px ";
+confirmPas.style.padding = "5px 15px";
+confirmPas.style.borderRadius = "5px ";
 LoginBtn.style.padding = "5px 85px";
 LoginBtn.style.borderRadius = "5px ";
 LoginBtn.style.backgroundColor = "blue ";
 LoginBtn.style.color = "white";
 
-let userObj = JSON.parse(localStorage.getItem("user"))
 
-
-Form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let obj = {
-    email: Address.value,
-    Password: Password.value,
-  };
-  
-  console.log(userObj);
-  if(userObj) {
-    window.location.href = 'http://127.0.0.1:5500/practise.30.11/home.html'
-  } else {
-    alert("User not found")
-  }
-});
+// let userInfo = {
+//   name: nameInput.value,
+//   email: emailInput.value,
+//   password: passwordInput.value
+// }
+// localStorage.setItem("user", JSON.stringify(userInfo))
